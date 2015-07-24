84638aabb9ec4411572b0a746da680821e4c52e8
Patrick Hooper <patrick.d.hooper@gmail.com>
2015-07-24 14:07:49 -0400

    Updated to floating button to IT Help

--- template-parts/content_wgiinformer-floating-buttons.php
+++ template-parts/content_wgiinformer-floating-buttons.php
@@ -11,7 +11,7 @@
     <i class="large mdi-action-assignment"></i>
   </a><!-- .btn-floating -->
   <ul>
-    <li><a href="https://wantmangroup.typeform.com/to/UktfqZ" class="typeform-share btn-floating blue tooltipped" data-position="left" data-delay="1" data-tooltip="Help" data-mode="2" target="_blank"><i class="large mdi-communication-live-help"></i></a></li>
+    <li><a href="https://wantmangroup.typeform.com/to/UktfqZ" class="typeform-share btn-floating blue tooltipped" data-position="left" data-delay="1" data-tooltip="IT Help" data-mode="2" target="_blank"><i class="large mdi-communication-live-help"></i></a></li>
     <li><a href="https://wantmangroup.typeform.com/to/QTjzlw" class="typeform-share btn-floating orange tooltipped" data-position="left" data-delay="1" data-tooltip="Feedback" data-mode="2" target="_blank"><i class="large mdi-communication-comment"></i></a></li>
   </ul>
 </div><!-- .fixed-action-btn -->
