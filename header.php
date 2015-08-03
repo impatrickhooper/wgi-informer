871a58abb46b25c82b733aa3a4fbed08befa6484
Patrick Hooper <patrick.d.hooper@gmail.com>
2015-08-03 11:16:21 -0400

    Added favicon

--- header.php
+++ header.php
@@ -25,6 +25,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
 <?php wp_head(); ?>
 
