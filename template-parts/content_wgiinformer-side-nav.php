689e667679280aa14575804c0514e7486bff26dd
Patrick Hooper <patrick.d.hooper@gmail.com>
2015-08-08 18:00:12 -0400

    Updated side nav image link styles

--- template-parts/content_wgiinformer-side-nav.php
+++ template-parts/content_wgiinformer-side-nav.php
@@ -19,24 +19,24 @@
   ?>
 
   <div class="side-nav_top <?php echo $user_profile_division; ?>">
-    <div class="side-nav_top-links">
-      <a href="<?php echo site_url(); ?>/logout" class="user-profile_sign-out user-profile_link"><i class="fa fa-sign-out"></i><span>sign out</span></a>
-      <a href="<?php echo site_url(); ?>/user" class="user-profile_account user-profile_link"><i class="fa fa-user"></i><span>profile</span></a>
-    </div>
-
     <?php
       /* If there's a user photo, load it here */
       if ($user_profile_photo != '') {
-        echo '<p class="user-profile_img clearfix"><a href="' . site_url() . '/user"><img src="' . site_url() . '/wp-content/uploads/ultimatemember/' . $user_profile_id . '/profile_photo-100.jpg"></a></p>';
+        echo '<p class="user-profile_img"><a href="' . site_url() . '/user"><img src="' . site_url() . '/wp-content/uploads/ultimatemember/' . $user_profile_id . '/profile_photo-100.jpg"></a></p>';
       }
       /* If no photo, use the default */
       else {
-        echo '<p class="user-profile_img clearfix"><a href="' . site_url() . '/user"><img src="' . get_stylesheet_directory_uri() . '/img/profile_photo-default.jpg"></a></p>';
+        echo '<p class="user-profile_img"><a href="' . site_url() . '/user"><img src="' . get_stylesheet_directory_uri() . '/img/profile_photo-default.jpg"></a></p>';
       }
     ?>
+
+    <div class="side-nav_top-links">
+      <a href="<?php echo site_url(); ?>/logout" class="user-profile_sign-out user-profile_link"><i class="fa fa-sign-out"></i><span>sign out</span></a>
+      <a href="<?php echo site_url(); ?>/user" class="user-profile_account user-profile_link"><i class="fa fa-user"></i><span>profile</span></a>
+    </div><!-- .side-nav_top-links -->
 
     <p class="user-profile_name"><?php echo $user_profile_name; ?></p>
     <p class="user-profile_email"><?php echo $user_profile_email; ?></p>
   </div>
   <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => '', 'menu_class' => 'side-nav_menu', 'container' => false ) ); ?>
-</div>
+</div><!-- #main-nav.side-nav -->
